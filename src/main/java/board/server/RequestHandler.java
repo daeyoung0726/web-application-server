3e08127d3365b08c9400d5e5f5d8d34840a148b8
DaeYoung0726 <pdy000726@naver.com>
2024-11-19 18:22:02 +0900

    refact: 코드 리팩토링

--- src/main/java/board/server/RequestHandler.java
+++ src/main/java/board/server/RequestHandler.java
@@ -1,18 +1,14 @@
 package board.server;
 
+import board.common.util.http.HttpRequest;
+import board.common.util.http.HttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -28,52 +24,20 @@ public class RequestHandler extends Thread {
                 connection.getInetAddress(), connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+            HttpRequest request = new HttpRequest(in);
+            HttpResponse response = new HttpResponse(out);
 
-            String line = br.readLine();
-            log.debug("request api : {}", line);
-
-            if (line == null)
-                return;
-
-            String[] tokens = line.split(" ");  // HTTP 메서드 및 url resource 분리
-
-            while (!line.equals("")) {
-                line = br.readLine();
-                log.debug("header : {}", line);
-            }
-
-            String url = tokens[1];
-            responseResource(out, url);
+            String path = getDefaultPath(request.getPath());
+            response.forward(path);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
 
-    private void responseResource(OutputStream out, String url) throws IOException {
-        DataOutputStream dos = new DataOutputStream(out);
-        byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
-        response200Header(dos, body.length);
-        responseBody(dos, body);
-    }
-
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.error(e.getMessage());
+    private String getDefaultPath(String path) {
+        if (path.equals("/")) {
+            return "/index.html";
         }
-    }
-
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
+        return path;
     }
 }
