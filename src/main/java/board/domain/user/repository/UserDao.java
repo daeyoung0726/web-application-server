d1dfbcf9170d60278d97e4e1a6128dfb7bd9d950
DaeYoung0726 <pdy000726@naver.com>
2024-11-21 03:32:52 +0900

    feat: User 로직 처리

--- src/main/java/board/domain/user/repository/UserDao.java
+++ src/main/java/board/domain/user/repository/UserDao.java
@@ -1,2 +1,40 @@
-package board.domain.user.repository;public class UserDao {
+package board.domain.user.repository;
+
+import board.database.ConnectionMaker;
+import board.database.DataBase;
+import board.domain.user.model.User;
+
+import java.util.List;
+
+public class UserDao {
+    private static final UserDao userDao = new UserDao();
+    private final DataBase<User> userDataBase;
+
+    private UserDao() {
+        this.userDataBase = ConnectionMaker.getUserDateBase();
+    }
+
+    public static UserDao getInstance() {
+        return userDao;
+    }
+
+    public void saveUser(User user) {
+        userDataBase.add(user);
+    }
+
+    public User findUserById(String username) {
+        return userDataBase.findById(username);
+    }
+
+    public List<User> findAllUsers() {
+        return userDataBase.findAll();
+    }
+
+    public void updateUser(String username, User user) {
+        userDataBase.updateById(username, user);
+    }
+
+    public void deleteUser(String username) {
+        userDataBase.deleteById(username);
+    }
 }
--- src/main/java/board/domain/user/service/UserService.java
+++ src/main/java/board/domain/user/service/UserService.java
@@ -1,2 +1,39 @@
-package board.domain.user.service;public class UserService {
+package board.domain.user.service;
+
+import board.domain.user.model.User;
+import board.domain.user.repository.UserDao;
+
+import java.util.List;
+
+public class UserService {
+    private static final UserService userService = new UserService();
+    private final UserDao userDao;
+
+    private UserService() {
+        this.userDao = UserDao.getInstance();
+    }
+
+    public static UserService getInstance() {
+        return userService;
+    }
+
+    public void saveUser(User user) {
+        userDao.saveUser(user);
+    }
+
+    public User findUserById(String username) {
+        return userDao.findUserById(username);
+    }
+
+    public List<User> findAllUsers() {
+        return userDao.findAllUsers();
+    }
+
+    public void updateUser(String username, User user) {
+        userDao.updateUser(username, user);
+    }
+
+    public void deleteUser(String username) {
+        userDao.deleteUser(username);
+    }
 }
