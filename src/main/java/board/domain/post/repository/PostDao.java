cf86e6ef5cb5efd6268561e6f7e69b6106427612
DaeYoung0726 <pdy000726@naver.com>
2024-11-22 02:56:47 +0900

    feat: Post dao, service 작성

--- src/main/java/board/domain/post/repository/PostDao.java
+++ src/main/java/board/domain/post/repository/PostDao.java
@@ -1,2 +1,40 @@
-package board.domain.post.repository;public class PostDao {
+package board.domain.post.repository;
+
+import board.database.ConnectionMaker;
+import board.database.DataBase;
+import board.domain.post.model.Post;
+
+import java.util.List;
+
+public class PostDao {
+    private static final PostDao postDao = new PostDao();
+    private final DataBase<Post> postDataBase;
+
+    private PostDao() {
+        this.postDataBase = ConnectionMaker.getPostDataBase();
+    }
+
+    public static PostDao getInstance() {
+        return postDao;
+    }
+
+    public Long savePost(Post post) {
+        return (Long) postDataBase.add(post);
+    }
+
+    public Post findPostById(Long id) {
+        return postDataBase.findById(id);
+    }
+
+    public List<Post> findAllPosts() {
+        return postDataBase.findAll();
+    }
+
+    public void updatePost(Long id, Post post) {
+        postDataBase.updateById(id, post);
+    }
+
+    public void deletePost(Long id) {
+        postDataBase.deleteById(id);
+    }
 }
--- src/main/java/board/domain/post/service/PostService.java
+++ src/main/java/board/domain/post/service/PostService.java
@@ -1,2 +1,48 @@
-package board.domain.post.service;public class PostService {
+package board.domain.post.service;
+
+import board.domain.post.model.Post;
+import board.domain.post.repository.PostDao;
+
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+public class PostService {
+    private static final PostService postService = new PostService();
+    private final PostDao postDao;
+
+    private PostService() {
+        this.postDao = PostDao.getInstance();
+    }
+
+    public static PostService getInstance() {
+        return postService;
+    }
+
+    public Long savePost(Post post) {
+        return postDao.savePost(decodeUser(post));
+    }
+
+    public Post findUserById(Long id) {
+        return postDao.findPostById(id);
+    }
+
+    public List<Post> findAllPosts() {
+        return postDao.findAllPosts();
+    }
+
+    public void updatePost(Long id, Post post) {
+        postDao.updatePost(id, post);
+    }
+
+    public void deletePost(Long id) {
+        postDao.deletePost(id);
+    }
+
+    private Post decodeUser(Post post) {
+        return new Post(
+                URLDecoder.decode(post.getTitle(), StandardCharsets.UTF_8),
+                URLDecoder.decode(post.getContent(), StandardCharsets.UTF_8)
+        );
+    }
 }
