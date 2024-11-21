33b9891dd0e01089d7a31505b48485f1a4cfc646
DaeYoung0726 <pdy000726@naver.com>
2024-11-22 02:57:35 +0900

    feat: Post Controller 매핑

--- src/main/java/board/server/RequestMapping.java
+++ src/main/java/board/server/RequestMapping.java
@@ -1,6 +1,11 @@
 package board.server;
 
 import board.common.controller.Controller;
+import board.domain.post.controller.CreatePostController;
+import board.domain.post.controller.DeletePostController;
+import board.domain.post.controller.ReadAllPostController;
+import board.domain.post.controller.ReadPostController;
+import board.domain.post.controller.UpdatePostController;
 import board.domain.user.controller.CreateUserController;
 import board.domain.user.controller.DeleteUserController;
 import board.domain.user.controller.LoginController;
@@ -13,9 +18,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class RequestMapping {
-    private static Map<String, Controller> controllers = new HashMap<>();
+    private static final Map<String, Controller> controllers = new HashMap<>();
 
     static {
+        addUserControllers();
+        addPostControllers();
+    }
+
+    private static void addUserControllers() {
         controllers.put("/users/sign-up", new CreateUserController());
         controllers.put("/users/sign-in", new LoginController());
         controllers.put("/users/sign-out", new LogoutController());
@@ -23,6 +33,14 @@ public class RequestMapping {
         controllers.put("/users/update", new UpdateUserController());
         controllers.put("/users/delete", new DeleteUserController());
         controllers.put("/users", new ReadAllUserController());
+    }
+
+    private static void addPostControllers() {
+        controllers.put("/posts/create", new CreatePostController());
+        controllers.put("/posts", new ReadAllPostController());
+        controllers.put("/posts/read", new ReadPostController());
+        controllers.put("/posts/update", new UpdatePostController());
+        controllers.put("/posts/delete", new DeletePostController());
     }
 
     public static Controller getController(String url) {
